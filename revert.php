cc910b810b1d235a54efcab72d7ad1613cc97d11
Hamroz Gavharov <62178569+hamroz@users.noreply.github.com>
2024-08-08 18:22:29 +0300

    Update revert.php

--- revert.php
+++ revert.php
@@ -1,9 +1,21 @@
-<?php 
+<?php
+/**
+ * Revert modifications made to the parent plugin
+ *
+ * This script reverts any modifications applied to the parent plugin directory
+ * by the child plugin. It uses functions defined in src/core.php to perform
+ * the revert operation.
+ */
+
+// Include core functions
 require 'src/core.php';
 
+// Set the operation type for logging
 $output->operation = "revert";
 
-//call the core function to revert changes
+// Call the core revert function
+// The true parameter enables logging of the revert operation
 revert(true);
 
+// Output the results
 require 'src/output.php';
